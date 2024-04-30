413e962f167b14847db2b4ccb5873676fec46531
jape@japes-macbook-air.local <tubino.jp@gmail.com>
2024-04-30 11:20:38 +0800

    first commit

---
+++ Homepage.js
@@ -0,0 +1,49 @@
+// image looping
+const loopImgArray = [
+    './mainResources/Discount.png',
+    '/mainResources/Voucher.png',
+    '/mainResources/Women shoes.png'
+];
+
+const imageElement = document.getElementById('loopingImages');
+let currentIndex = 0;
+function imageLooping() {
+    imageElement.classList.add('hidden');
+    
+    setTimeout(() => {
+        imageElement.src = loopImgArray[currentIndex];
+        imageElement.classList.remove('hidden');
+        currentIndex = (currentIndex + 1) % loopImgArray.length;
+    }, 500); 
+}
+
+imageLooping();
+
+setInterval(imageLooping, 4000);
+
+const dotsContainer = document.getElementById('dotsContainer');
+
+for (let i = 0; i < loopImgArray.length; i++) {
+    const dot = document.createElement('span');
+    dot.classList.add('dot');
+    dot.setAttribute('data-index', i);
+    dotsContainer.appendChild(dot);
+}
+
+const dots = document.querySelectorAll('.dot');
+
+function dotClickHandler(event) {
+    const dotIndex = event.target.getAttribute('data-index');
+    currentIndex = parseInt(dotIndex);
+    imageLooping();
+}
+
+dots.forEach(dot => {
+    dot.addEventListener('click', dotClickHandler);
+});
+
+
+function updateActiveDot() {
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[currentIndex].classList.add('active');
+}
