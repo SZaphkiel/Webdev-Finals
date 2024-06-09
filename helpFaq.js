29b5e4f7c5758c64f4b635698cd350e59b7e6764
jape@japes-macbook-air.local <tubino.jp@gmail.com>
2024-06-09 14:19:20 +0800

    css js

--- helpFaq.js
+++ helpFaq.js
@@ -1,22 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let qCards = document.querySelectorAll('.qCards');
-
-    for (let i = 0; i < qCards.length; i++) {
-        qCards[i].addEventListener('click', function(event) {
-            let qNa = this.querySelector('.qNa');
-            let qArrow = this.querySelector('.qArrow');
-
-            if (qNa.classList.contains('show')) {
-                qNa.classList.remove('show');
-            } else {
-                qNa.classList.add('show');
-            }
-
-            if (qArrow.classList.contains('rotated')) {
-                qArrow.classList.remove('rotated');
-            } else {
-                qArrow.classList.add('rotated');
-            }
-        });
-    }
+document.querySelectorAll('.qCards').forEach(card => {
+    card.addEventListener('click', () => {
+        const answer = card.querySelector('.qNa');
+        const arrow = card.querySelector('.qArrow');
+        if (answer.classList.contains('show')) {
+            answer.classList.remove('show');
+            arrow.classList.remove('rotated');
+        } else {
+            document.querySelectorAll('.qNa').forEach(answer => answer.classList.remove('show'));
+            document.querySelectorAll('.qArrow').forEach(arrow => arrow.classList.remove('rotated'));
+            answer.classList.add('show');
+            arrow.classList.add('rotated');
+        }
+    });
 });
--- mainShopping.js
+++ mainShopping.js
@@ -1,4 +1,29 @@
-const buttons = document.getElementById('')
-function pageChange(params) {
-    document.location.href='productPage';
+const productButtons = document.querySelectorAll('.cardBtn');
+
+const buttonArray = [];
+
+for (let i = 0; i < productButtons.length; i++) {
+    buttonArray.push(productButtons[i]);
+}
+
+buttonArray.forEach((button, index) => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault(); 
+        handleButtonClick(index); 
+        document.location.href = 'productPage.html'; 
+    });
+});
+
+const productName = document.getElementById('productName');
+
+function handleButtonClick(index) {
+    console.log("Button clicked:", index);
+    switch (index) {
+        case 0:
+            productName.innerHTML += "Bagong shoes"
+            break;
+        case 1:
+            // Add functionality for button 2
+            break;
+    }
 }
